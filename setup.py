d05ea220d3298a791a10a33acde15edf58c4d9e7
walidbedhiafi <ontocontext@gmail.com>
2016-08-23 00:45:22 +0100

    annotation fixed bugs+setup

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 setup(
     name = 'OntoContext',
     packages = ['OntoContext'],
-    version = '0.10',  
+    version = '0.11',  
     description = 'Beta Version',
     author = 'Walid Bedhiafi',
     author_email = 'ontocontext@gmail.com',
     url = 'https://github.com/walidbedhiafi/OntoContext1',
-    download_url = 'https://github.com/walidbedhiafi/OntoContext1/archive/0.10.tar.gz',
+    download_url = 'https://github.com/walidbedhiafi/OntoContext1/archive/0.11.tar.gz',
     keywords = ['Bioinformatics', 'textmining' , 'Annotating', 'Ontology', 'biomedicaltext'],
     classifiers = [],
 )
